2a33eae8a2ff02b3bb81438bdf8f8db0642faf3d
OlgaNaranovich <naranovicholga@gmail.com>
2018-10-04 00:16:19 +0300

    advances-zeros

--- src/index.js
+++ src/index.js
@@ -12,21 +12,18 @@ module.exports = function getZerosCount(number, base) {
 		}
 		i++;
 	}
-	// console.log(simpleMults);
 
 	let bigNumber = simpleMults[0];
 	for (let i = 0, len = simpleMults.length; i < len; i++) {
 		if (simpleMults[i] <= simpleMults[i + 1]) {
 			bigNumber = simpleMults[i + 1];
 		} 
-		// console.log(bigNumber);
 	}
-
+	let countRepeats = simpleMults.filter(item => item === bigNumber).length;
 	let zerosCount = 0;
   while (number > 0) {
   	number = Math.floor(number / bigNumber);
   	zerosCount += number;
   }
-  return zerosCount;
-
+  return Math.floor(zerosCount / countRepeats);
 }
