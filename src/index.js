f2ea003133a56c3e81043ce7f07613734a7076cf
OlgaNaranovich <naranovicholga@gmail.com>
2018-10-03 23:37:51 +0300

    advances-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let b = base;
+
+	let i = 2;
+	const simpleMults = [];
+	while (i <= b) {
+		if (b % i === 0) {
+			simpleMults.push(i);
+			b = b / i;
+			continue;
+			if (b === 1) {break;}
+		}
+		i++;
+	}
+	// console.log(simpleMults);
+
+	let bigNumber = simpleMults[0];
+	for (let i = 0, len = simpleMults.length; i < len; i++) {
+		if (simpleMults[i] <= simpleMults[i + 1]) {
+			bigNumber = simpleMults[i + 1];
+		} 
+		// console.log(bigNumber);
+	}
+
+	let zerosCount = 0;
+  while (number > 0) {
+  	number = Math.floor(number / bigNumber);
+  	zerosCount += number;
+  }
+  return zerosCount;
+
 }
